398920d94d7275ee8d6e976e19fccd7ba0ee6419
thuandc21v7n546@vlvh.ctu.edu.vn <thuandc21v7n546@vlvh.ctu.edu.vn>
2025-09-29 21:58:18 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-// chạy server
-app.listen(config.app.port, () => {
-  console.log(`Server dang chay tai http://localhost:${config.app.port}`);
-});
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to the database!");
+        
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error("Cannot connect to the database!", error);
+        process.exit(1);
+    }
+}
+
+startServer();
